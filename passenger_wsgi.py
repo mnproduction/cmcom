0331b2a0c728cf3243fb4c66ec4d82eea822d716
MYKOLA NOVOSOLOV <sckenich@gmail.com>
2023-11-08 18:45:29 +0200

    Remove unnecessary import statement in passenger_wsgi.py

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from cmprj.cmprj.wsgi import application
+
+# .wsgi import application
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+# wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
+# application = wsgi.application
